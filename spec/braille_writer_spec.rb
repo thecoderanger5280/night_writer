30e730e827578f4ec53488c9877d74df58fc0525
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-22 15:33:45 -0600

    fix spelling mistakes

--- spec/braille_writer_spec.rb
+++ spec/braille_writer_spec.rb
@@ -1,12 +1,24 @@
 require 'rspec'
 require './lib/braille_writer.rb'
 
-RSpec.describe BraileWriter do
+RSpec.describe BrailleWriter do
   describe '#initialize' do
     it 'exists' do
-      writer = BraileWriter.new('message.txt', 'braille.txt')
+      writer = BrailleWriter.new(['message.txt', 'braille.txt'])
 
-      expect(writer).to be_an_instance_of(BraileWriter)
+      expect(writer).to be_an_instance_of(BrailleWriter)
+    end
+
+    it 'has a file path to read from' do
+      writer = BrailleWriter.new(['message.txt', 'braille.txt'])
+
+      expect(writer.read_file).to eq('message.txt')
+    end
+
+    it 'has a file path to write to' do
+      writer = BrailleWriter.new(['message.txt', 'braille.txt'])
+
+      expect(writer.write_file).to eq('braille.txt')
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
 require 'simplecov'
 SimpleCov.start
 
-require_relative 'braile_writer_spec'
+require_relative 'braille_writer_spec'
