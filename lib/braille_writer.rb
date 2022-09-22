0810136ce07dc1de32b8798991b7ce4401f6a76a
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-22 16:02:11 -0600

    finish output method so it outputs file length and file name

--- lib/braille_writer.rb
+++ lib/braille_writer.rb
@@ -4,9 +4,15 @@ class BrailleWriter
   def initialize(files)
     @read_file = files[0]
     @write_file = files[1]
+    @message_file = File.open(@read_file, "r")
+    @braille_file = File.open(@write_file, "w")
   end
 
   def output
-    "Created '#{@write_file}' containing 256 characters"
+    "Created '#{@write_file}' containing #{read_file_length} characters"
+  end
+
+  def read_file_length
+    @message_file.read.length
   end
 end
