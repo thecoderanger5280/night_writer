5bbc997c503b9cb964be472e084c8ad006fd1a08
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-22 14:58:40 -0600

    add test to check if it has a file path to read from

--- spec/braile_writer_spec.rb
+++ spec/braile_writer_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe BraileWriter do
 
       expect(writer).to be_an_instance_of(BraileWriter)
     end
+
+    it 'has a read file name' do
+      writer = BraileWriter.new
+
+      expect(writer.read_file).to be_an_instance_of(String)
+    end
   end
 end
