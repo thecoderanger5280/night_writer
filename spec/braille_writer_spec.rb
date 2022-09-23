1bfefdbdb6a327da67661954d976c084c0d14da2
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-23 10:19:55 -0600

    add functionality for full alphabet

--- spec/braille_writer_spec.rb
+++ spec/braille_writer_spec.rb
@@ -32,13 +32,21 @@ RSpec.describe BrailleWriter do
   end
 
   describe '#braille_converter' do
-    it 'can write to the new file' do
+    it 'can convert a letter to braille' do
       writer = BrailleWriter.new(['message.txt', 'braille.txt'])
       allow(writer).to receive(:message_file).and_return(double("message_file"))
-      allow(writer.message_file).to receive(:read).and_return('hh')
+      allow(writer.message_file).to receive(:read).and_return('h')
 
-      expect(writer.braille_converter(writer.message_file.read)).to eq("0.\n00\n..")
+      expect(writer.braille_converter(writer.message_file.read)).to eq("0.\n00\n..\n")
       # require 'pry'; binding.pry
+    end
+
+    it 'can convert a whole string to braille' do
+      writer = BrailleWriter.new(['message.txt', 'braille.txt'])
+      allow(writer).to receive(:message_file).and_return(double("message_file"))
+      allow(writer.message_file).to receive(:read).and_return('hello world')
+
+      expect(writer.braille_converter(writer.message_file.read)).to eq("0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n")
     end
   end
 end
